0e70d356fc17b824efa17dd2e2e9b56e8c4b5d6a
Ana Letícia Paiva <analeticia.s.paiva@gmail.com>
2022-05-05 15:15:33 -0300

    alteração para sync/await
    Alteração do script para a utilização de async/await. Inclusão de alerta de erro.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,24 @@ const numConselho = document.querySelector ('#id-conselho');
 const texto = document.querySelector ('#texto');
 const geraNovoConselho = document.querySelector ('.btn-gera-conselho');
 
-function geraConselho (){
-    
-    fetch (`https://api.adviceslip.com/advice`)
-    
-    .then (resposta => {
-        return resposta.json();
-    }) 
-    .then (data => {
-        const numero = data.slip;
-        numConselho.textContent = `${numero.id}`
-        const txt = data.slip;
-        texto.textContent = `"${txt.advice}"`
-    })
+async function geraConselho (){
+    try { 
+        const data = await fetch (`https://api.adviceslip.com/advice`)
+        .then (resposta => {
+            return resposta.json();
+        })
+            if (data){
+                const numero = data.slip;
+                numConselho.textContent = `${numero.id}`
+                const txt = data.slip;
+                texto.textContent = `"${txt.advice}"`
+            }
+    }
+    catch (err){
+        window.alert("Não foi possível gerar um novo conselho!");
+        console.log(err)
+    }
 }
-
 geraNovoConselho.addEventListener("click", () => {
     geraConselho();
 });
