3b22184a5a4c2e0219387407619aa4a4d4243b71
Ana Letícia Paiva <analeticia.s.paiva@gmail.com>
2022-05-06 15:29:44 -0300

    incrementação do setTimeout
    Foi feita a incrementação do setTimeout, para que o botão de gerar novo conselho, seja habilitado somente de 2 em 2 segundos (junto com a API).

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ const numConselho = document.querySelector ('#id-conselho');
 const texto = document.querySelector ('#texto');
 const geraNovoConselho = document.querySelector ('.btn-gera-conselho');
 
+alert("Bem vindo! Clique no botão verde para gerar seu conselho diário!");
+
+
 async function geraConselho (){
-    try { 
+document.querySelector ('.btn-gera-conselho').disabled = true
+    try {  
+        setTimeout(function() {
+            document.querySelector ('.btn-gera-conselho').disabled = false
+            }, 2000);
+            console.log(setTimeout);
+
         const data = await fetch (`https://api.adviceslip.com/advice`)
         .then (resposta => {
-            return resposta.json();
+                return resposta.json();
         })
             if (data){
                 const numero = data.slip;
@@ -14,14 +23,15 @@ async function geraConselho (){
                 const txt = data.slip;
                 texto.textContent = `"${txt.advice}"`
             }
-    }
+        }
     catch (err){
-        window.alert("Não foi possível gerar um novo conselho!");
+        alert("Não foi possível gerar um novo conselho!");
         console.log(err)
     }
 }
+    
 geraNovoConselho.addEventListener("click", () => {
     geraConselho();
 });
 
-geraConselho();
+
